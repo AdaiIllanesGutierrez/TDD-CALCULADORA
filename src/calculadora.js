9f1ddbf9ae32540b0c477f2af93c2a5a04724a33
AdaiIllanesGutierrez <adai.illanes@ucb.edu.bo>
2023-03-28 06:47:27 -0400

    convertir un string a un entero

--- src/calculadora.js
+++ src/calculadora.js
@@ -1,20 +1,8 @@
 
-function Calculadora(texto) {
-    if(texto=="") return 0;
-
-    let numeros=texto.split(",");
-    if (numeros.legth==1){
-        return parseInt(texto);
-
-    }else {
-        let sumar=0;
-        for(let i=0;i<numeros.legth;i++){
-            sumar=sumar+parseInt(numeros[i]);
-        }
-
-    }
-
-    return sumar;
+function Calculadora(cadena) {
+    if(cadena=="") return 0;
+   
+    else return parseInt(cadena);
 
   }
   
--- src/calculadora.spec.js
+++ src/calculadora.spec.js
@@ -4,13 +4,26 @@ describe("Calculadora", () => {
   it("calcular una cadena vacia", () => {
     expect(calculadora("")).toEqual(0);
   });
-  xit("convertir un string a un entero", () => {
+  it("convertir un string a un entero", () => {
     expect(calculadora("1")).toEqual(1);
   });
-  xit("separar", () => {
+  xit("separar segun la coma y sumar los numeros ", () => {
     expect(calculadora("1,2")).toEqual(3);
   });
-  xit("calcular una cadena vacia", () => {
-    expect(calculadora("")).toEqual(0);
+  xit("sumar 2 o mas valores separados por una coma", () => {
+    expect(calculadora("1,2,3,4,5")).toEqual(15);
   });
+  xit("sumar 2 valores separados por guion", () => {
+    expect(calculadora("1-2")).toEqual(3);
+  });
+  xit("deberia devolver sumar mas de 2 valores por guion", () => {
+    expect(calculadora("1-2-3-4-5")).toEqual(15);
+  });
+  xit("deberia devolver sumar mas de 2 valores separado por guion o coma", () => {
+    expect(calculadora("1,2,3-4-5")).toEqual(15);
+  });
+  xit("deberia devolver suma de 2 valores separados por guion o coma o un delimitardor especial ", () => {
+    expect(calculadora("//[;] 6,3-2;1")).toEqual(12);
+  });
+
 });
